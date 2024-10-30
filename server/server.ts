890f27a668667a2731708f3cc7e5363f9b8b86f8
makim0939 <95673492+makim0939@users.noreply.github.com>
2024-10-31 02:04:54 +0900

    initialize: project initialize

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,11 @@
-import http from "http";
-import { Server } from "socket.io";
+import http from 'http';
+import { Server } from 'socket.io';
 
 const server = http.createServer();
 
-server.listen(3000, () => console.log("Server running on port 3000"));
+server.listen(3000, () => console.log('Server running on port 3000'));
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: '*',
   },
 });
