7698271b820d2970003f452a539c24bf0b6e58c0
makim0939 <95673492+makim0939@users.noreply.github.com>
2024-11-04 16:11:30 +0900

    feat: display socketio connection

--- display/src/App.tsx
+++ display/src/App.tsx
@@ -1,16 +1,6 @@
-import { useEffect } from "react";
-import io from "socket.io-client";
+import useSocket from './hooks/useSocket';
 function App() {
-  useEffect(() => {
-    const SERVER_URL = "http://" + location.hostname + ":3000";
-    const socket = io(SERVER_URL);
-    socket.on("connect", () => {
-      console.log("Connected to server");
-    });
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
+  const socket = useSocket();
   return (
     <>
       <h2 className=" text-red-600">Live Interactive Map - Display</h2>
