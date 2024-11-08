83cdc3a727c072ecac16a65973a7c955be70d7c0
makim0939 <95673492+makim0939@users.noreply.github.com>
2024-11-08 11:44:29 +0900

    refactor: separate position calculation from sendViewRect.ts

--- client/src/socketio/sendViewRect.ts
+++ client/src/socketio/sendViewRect.ts
@@ -1,13 +1,11 @@
 import { Socket } from 'socket.io-client';
+import { Rect } from '../types';
 
-const sendViewRect = (socket: Socket) => {
+const sendViewRect = (socket: Socket, viewRect: Rect) => {
   const colorInput = document.getElementById('colorInput') as HTMLInputElement;
   socket.emit('view-rect', {
-    left: scrollX,
-    top: scrollY,
-    width: innerWidth,
-    height: innerHeight,
-    color: colorInput ? colorInput.value : '#00FF00',
+    ...viewRect,
+    color: colorInput ? colorInput.value : '#00ff00',
   });
 };
 export default sendViewRect;
