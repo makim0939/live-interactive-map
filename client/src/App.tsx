ade030cd34896c6790ca8bd67306a5c198a7c0c1
makim0939 <95673492+makim0939@users.noreply.github.com>
2024-11-08 15:02:23 +0900

    feat: display booth card

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,47 @@
+import { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
-import MapContents from './components/MapContents';
-import sendViewRect from './socketio/sendViewRect';
 import { BoothInfo } from './types.ts';
+import sendViewRect from './socketio/sendViewRect';
 import getBoothInViewport from './getBoothInViewport.ts';
 import { addDummyBooths } from './debugFunctinos.ts';
+import MapContents from './components/MapContents';
+import BoothCard from './components/BoothCard.tsx';
 
 const SERVER_URL = import.meta.env.VITE_SERVER_URL as string;
 const socket = io(SERVER_URL);
 
 const booths: BoothInfo[] = [];
-// テスト用にダミーブースを追加
-addDummyBooths(booths);
-//
-
-socket.on('booth', (booth) => {
+socket.on('booth', (booth: BoothInfo) => {
   booths.push(booth);
 });
 
-const handleScroll = () => {
-  const viewRect = { left: scrollX, top: scrollY, width: innerWidth, height: innerHeight };
-  sendViewRect(socket, viewRect);
-  const boothInViewport = getBoothInViewport(booths, viewRect);
-  if (!boothInViewport) return;
-  //TODO:ブースカードの表示
-};
-addEventListener('scroll', handleScroll);
 function App() {
+  const [boothInViewport, setBoothInViewport] = useState<BoothInfo | undefined>(undefined);
+  useEffect(() => {
+    //DELETE:テスト用にダミーブースを追加
+    addDummyBooths(booths);
+    //
+
+    const handleScroll = () => {
+      const viewRect = { left: scrollX, top: scrollY, width: innerWidth, height: innerHeight };
+      sendViewRect(socket, viewRect);
+      const booth = getBoothInViewport(booths, viewRect);
+      setBoothInViewport(booth);
+    };
+    addEventListener('scroll', handleScroll);
+    return () => {
+      //DELETE:テスト用にダミーブースを削除
+      booths.splice(0);
+      //
+      removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return (
     <>
       <MapContents />
+      {boothInViewport && (
+        <BoothCard title={boothInViewport.title} description={boothInViewport.description} />
+      )}
     </>
   );
 }
