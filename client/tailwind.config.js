34ac93b7c7c52b87272acca3e95fd0cebe67e3a9
makim0939 <95673492+makim0939@users.noreply.github.com>
2024-11-08 14:46:01 +0900

    fix: change tailwind color white→#f5f5f2, black→#252528

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: '#252528',
+        white: '#f5f5f2',
+      },
+    },
   },
   plugins: [],
 };
