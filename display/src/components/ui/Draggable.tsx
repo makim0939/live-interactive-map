3a274d15019c788493c6203d6ac57f090fdf4d2d
makim0939 <95673492+makim0939@users.noreply.github.com>
2024-11-11 22:16:29 +0900

    fix: stabilize draggable-component behaviore

--- display/src/components/ui/Draggable.tsx
+++ display/src/components/ui/Draggable.tsx
@@ -1,44 +1,56 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 const Draggable = ({ children }: { children: React.ReactNode }) => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
   const [isDragging, setIsDragging] = useState(false);
   const [startPosition, setStartPosition] = useState({ x: 0, y: 0 });
+  const [endPosition, setEndPosition] = useState({ x: 0, y: 0 });
 
   const handleMouseDown = (event: React.MouseEvent) => {
+    if (isDragging) return;
     setIsDragging(true);
     setStartPosition({
-      x: event.clientX - position.x,
-      y: event.clientY - position.y,
+      x: event.clientX,
+      y: event.clientY,
     });
-  };
-  const handleMouseMove = (event: React.MouseEvent) => {
-    if (!isDragging) return;
-    setPosition({
-      x: event.clientX - startPosition.x,
-      y: event.clientY - startPosition.y,
-    });
-  };
-  const handleMouseUp = () => {
-    setIsDragging(false);
   };
 
+  const handleMouseMove = useCallback(
+    (event: React.MouseEvent | MouseEvent) => {
+      if (!isDragging) return;
+      const DeltaX = event.clientX - startPosition.x;
+      const DeltaY = event.clientY - startPosition.y;
+      setPosition({
+        x: endPosition.x + DeltaX,
+        y: endPosition.y + DeltaY,
+      });
+    },
+    [isDragging, startPosition, endPosition],
+  );
+  const handleMouseUp = () => {
+    setIsDragging(false);
+    setEndPosition({ x: position.x, y: position.y });
+  };
+
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, [handleMouseMove]);
+
   return (
-    <>
+    <div className=" w-[100vw] h-[100vh] ">
       <div
         onMouseDown={handleMouseDown}
         onMouseMove={handleMouseMove}
         onMouseUp={handleMouseUp}
-        style={{ left: position.x, top: position.y }}
-        className=" absolute w-fit z-10 cursor-pointer"
+        style={{ transform: `translate(${position.x}px, ${position.y}px)` }}
+        className=" absolute top-0 left-0 w-fit z-10 cursor-pointer origin-top-left"
       >
         {children}
       </div>
-      <div
-        onMouseMove={handleMouseMove}
-        className=" fixed left-0 top-0 z-0 w-[100vw] h-[100vh]"
-      ></div>
-    </>
+    </div>
   );
 };
 
