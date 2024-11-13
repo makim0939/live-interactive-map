aa1b1c3d4efdc52fae2619ecf48905dc8063d565
makim0939 <95673492+makim0939@users.noreply.github.com>
2024-11-13 17:53:52 +0900

    feat: icon components

---
+++ display/src/components/icon/AddIcon.tsx
@@ -0,0 +1,25 @@
+import { IconProps } from './icon.types';
+
+const AddIcon = (iconProps: IconProps) => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="#252528"
+      xmlns="http://www.w3.org/2000/svg"
+      style={iconProps.style}
+    >
+      <g clipPath="url(#clip0_9_435)">
+        <path d="M21 11.2H12.7V3H11.2V11.2H3V12.7H11.2V21H12.7V12.7H21V11.2Z" />
+      </g>
+      <defs>
+        <clipPath id="clip0_9_435">
+          <rect width="24" height="24" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default AddIcon;
