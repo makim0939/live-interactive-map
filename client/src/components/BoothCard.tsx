b49e3f472a2a61689b26bc22bf4b80d814527b75
makim0939 <95673492+makim0939@users.noreply.github.com>
2024-11-18 23:46:07 +0900

    feat: favorite button view

--- client/src/components/BoothCard.tsx
+++ client/src/components/BoothCard.tsx
@@ -1,10 +1,49 @@
+import { useRef, useState } from "react";
+import FavoriteFillIcon from "./icon/FavoriteFillIcon";
+import FavoriteStrokeIcon from "./icon/FavoriteStrokeIcon";
+
 const BoothCard = ({ name, description }: { name: string; description: string }) => {
+  const [isFavoriteClicked, setIsFavoriteClicked] = useState(false);
+  const favFillIconRef = useRef<SVGSVGElement>(null);
+
+  const FavAnimation = async () => {
+    if (!favFillIconRef.current) return;
+    favFillIconRef.current.style.transform = "scale(1.4)";
+    setTimeout(() => {
+      if (!favFillIconRef.current) return;
+      favFillIconRef.current.style.transform = "scale(1)";
+    }, 200);
+  };
+  const onFavButtonClick = () => {
+    setIsFavoriteClicked(true);
+    FavAnimation();
+  };
   return (
-    <div className=" fixed bottom-4 z-10 w-[96%] h-20 mx-[2%] p-4 bg-bgwhite rounded-md flex flex-col justify-center ">
-      <p className=" text-lg">
-        <b>{name}</b>
-      </p>
-      <p className=" text-textgray truncate">{description}</p>
+    <div className=" fixed bottom-4 z-10 w-[96vw] h-20 mx-[2%] p-4  bg-bgwhite rounded-md flex justify-around items-center ">
+      <div className=" w-[80vw] flex flex-col justify-center ">
+        <p className=" text-lg">
+          <b>{name}</b>
+        </p>
+        <p className=" text-textgray truncate">{description}</p>
+      </div>
+      <button
+        type="button"
+        onClick={onFavButtonClick}
+        className=" w-fit h-fit pt-2 [&_svg]:duration-100 [&_svg]:ease-in-out"
+      >
+        {isFavoriteClicked ? (
+          <FavoriteFillIcon
+            ref={favFillIconRef}
+            style={{
+              width: 32,
+              height: 32,
+              fill: "#ee3311",
+            }}
+          />
+        ) : (
+          <FavoriteStrokeIcon style={{ width: 32, height: 32, fill: "#757578" }} />
+        )}
+      </button>
     </div>
   );
 };
