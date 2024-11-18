050b0ba58f57efd956b276b5371e5088a64b9662
makim0939 <95673492+makim0939@users.noreply.github.com>
2024-11-18 16:45:10 +0900

    fix: change types to common with display-side

--- client/src/getBoothInViewport.ts
+++ client/src/getBoothInViewport.ts
@@ -5,29 +5,23 @@
 - ブースの中心と画面の中心の距離が最も近いブースをBoothInViewportとして判定する.
   その距離がブースの対角線の長さより大きい場合undefined.
 */
-import type { BoothInfo, Rect } from "./types";
+import type { Booth, Rect } from "./types";
 
 const THRESHOLD_OCCUPANCY = 0.3;
 
 //採用した手法
-const getBoothInViewport = (booths: BoothInfo[], viewRect: Rect): BoothInfo | undefined => {
+const getBoothInViewport = (booths: Booth[], viewRect: Rect): Booth | undefined => {
   const viewportOccupancies = booths.map((booth) => {
-    if (
-      viewRect.left + viewRect.width < booth.area.left ||
-      booth.area.left + booth.area.width < viewRect.left
-    )
+    if (viewRect.left + viewRect.width < booth.left || booth.left + booth.width < viewRect.left)
       return 0;
-    if (
-      viewRect.top + viewRect.height < booth.area.top ||
-      booth.area.top + booth.area.height < viewRect.top
-    )
+    if (viewRect.top + viewRect.height < booth.top || booth.top + booth.height < viewRect.top)
       return 0;
     const w =
-      Math.min(viewRect.left + viewRect.width, booth.area.left + booth.area.width) -
-      Math.max(viewRect.left, booth.area.left);
+      Math.min(viewRect.left + viewRect.width, booth.left + booth.width) -
+      Math.max(viewRect.left, booth.left);
     const h =
-      Math.min(viewRect.top + viewRect.height, booth.area.top + booth.area.height) -
-      Math.max(viewRect.top, booth.area.top);
+      Math.min(viewRect.top + viewRect.height, booth.top + booth.height) -
+      Math.max(viewRect.top, booth.top);
     return (w * h) / (viewRect.width * viewRect.height);
   });
   const maxOccupancy = Math.max(...viewportOccupancies);
@@ -35,34 +29,25 @@ const getBoothInViewport = (booths: BoothInfo[], viewRect: Rect): BoothInfo | un
   return booths[viewportOccupancies.indexOf(maxOccupancy)];
 };
 
-const getBoothInViewportByOccupancy = (
-  booths: BoothInfo[],
-  viewRect: Rect,
-): BoothInfo | undefined => {
+const getBoothInViewportByOccupancy = (booths: Booth[], viewRect: Rect): Booth | undefined => {
   const viewportOccupancies = booths.map((booth) => {
-    if (
-      viewRect.left + viewRect.width < booth.area.left ||
-      booth.area.left + booth.area.width < viewRect.left
-    )
+    if (viewRect.left + viewRect.width < booth.left || booth.left + booth.width < viewRect.left)
       return 0;
-    if (
-      viewRect.top + viewRect.height < booth.area.top ||
-      booth.area.top + booth.area.height < viewRect.top
-    )
+    if (viewRect.top + viewRect.height < booth.top || booth.top + booth.height < viewRect.top)
       return 0;
     const w =
-      Math.min(viewRect.left + viewRect.width, booth.area.left + booth.area.width) -
-      Math.max(viewRect.left, booth.area.left);
+      Math.min(viewRect.left + viewRect.width, booth.left + booth.width) -
+      Math.max(viewRect.left, booth.left);
     const h =
-      Math.min(viewRect.top + viewRect.height, booth.area.top + booth.area.height) -
-      Math.max(viewRect.top, booth.area.top);
+      Math.min(viewRect.top + viewRect.height, booth.top + booth.height) -
+      Math.max(viewRect.top, booth.top);
     return (w * h) / (viewRect.width * viewRect.height);
   });
   const maxOccupancy = Math.max(...viewportOccupancies);
   if (maxOccupancy < THRESHOLD_OCCUPANCY) return;
   return booths[viewportOccupancies.indexOf(maxOccupancy)];
 };
-const getBoothInViewportByDistance = (booths: BoothInfo[], viewRect: Rect) => {
+const getBoothInViewportByDistance = (booths: Booth[], viewRect: Rect) => {
   const checkBoothInView = (dist: number) => {
     const viewportDiagonalLength = Math.sqrt(viewRect.width ** 2 + viewRect.height ** 2);
     return dist < viewportDiagonalLength / 2;
@@ -73,8 +58,8 @@ const getBoothInViewportByDistance = (booths: BoothInfo[], viewRect: Rect) => {
   };
   const dists = booths.map((booth) => {
     const boothCenter = {
-      x: booth.area.left + booth.area.width / 2,
-      y: booth.area.top + booth.area.height / 2,
+      x: booth.left + booth.width / 2,
+      y: booth.top + booth.height / 2,
     };
     return Math.sqrt(
       (boothCenter.x - viewportCenter.x) ** 2 + (boothCenter.y - viewportCenter.y) ** 2,
